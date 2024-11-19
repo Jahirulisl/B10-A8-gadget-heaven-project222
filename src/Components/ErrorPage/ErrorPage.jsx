e626a3fe33c36a1e3c22ee922831586fdd2b78f5
jahir <nockjahirul@gmail.com>
2024-11-19 17:52:46 +0600

    add ErrorPage component

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="text-5xl text-center m-4">
+        <h2>Page Not Found</h2>
+        <br />
+        <p>404</p>
+    </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './Components/Root/Root';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root></Root>
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>
+
   },
 ]);
 
